24acd3fbc02cd45a88db50b00444135f294d1306
decendman <decendman@gmail.com>
2026-02-24 11:07:33 +0530

    added sortting file

--- Main.java
+++ Main.java
@@ -3,25 +3,25 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
+        int number = sc.nextInt();
 
-        int[][] array = new int[rows][rows];
-
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j <= i; j++) {
-                if(j == 0 || j == i) {
-                    array[i][j] = 1;
-                } else {
-                    array[i][j] = array[i-1][j-1] + array[i-1][j];
-                }
-            }
+        int sign = 0;
+        if(number < 0) {
+            sign = -1;
+        } else {
+            sign = 1;
         }
 
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j <= i; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
+        int positiveNumber = number;
+        if(number < 0) {
+            positiveNumber = number * -1;
+        }
+
+        int reverse = 0;
+        while (positiveNumber != 0) {
+            int digit = positiveNumber % 10;
+            reverse = reverse * 10 + digit;
+            positiveNumber /= 10;
         }
         sc.close();
     }
